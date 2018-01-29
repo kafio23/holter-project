bd4225b7258c624f77d94a9958e8f5c1daad4400
Fiorella <dkafio@gmail.com>
2018-01-29 01:52:58 +0000

    Se agrego Diferencias por variabilidad R-R y Mean R-R

--- matlab/script28_enero.m
+++ matlab/script28_enero.m
@@ -4,8 +4,8 @@ clear all; close all; clc;
 
 %% Se adquiere la SEÑAL
 signal = load('nuevo4sinxy.csv');
-x = signal(1:10000,1);
-y = signal(1:10000,2);
+x = signal(:,1);%(1:15000,1);
+y = signal(:,2);%(1:15000,2);
 
 % Frecuencia de Muestreo
 Fs = 300; % (Hz)
@@ -104,25 +104,67 @@ sobra_bloque    = tiempo_total/segundos_bloque;
 % contador de figures
 cont_fig = 2;
 
+% ultimo loop
+last_loop = false;
+
+% Para R-R
+rr_values_all = []
+rr_mean_values_all = [];
+rr_mean_prom = 0
+
+% PROCESAMIENTO:
+
 if tiempo_total > segundos_bloque % Por encima de 10 segundos (tiempo total)
     
     for i = (x_inicio : segundos_bloque : x_final) % Va de 10 en 10 segs
         
-        % Datos de BLOQUE
-        indice_mayores = (i <= t);
-        t_bloque_parcial = t(indice_mayores); % Para t
-        y_bloque_parcial = y(indice_mayores); % Para y
-        indice_menores = (t_bloque_parcial < (i + segundos_bloque));
-        t_bloque = t_bloque_parcial(indice_menores);  % Para t
-        y_bloque = y_bloque_parcial(indice_menores);  % Para y
+        % EVITAR bloque menor a 5 segundos
+        ultimate_i = i + segundos_bloque; % Prox bloque
+        if (ultimate_i < x_final) && ((x_final - ultimate_i) < (segundos_bloque/2))
+                  
+            % Datos de BLOQUE
+            indice_mayores = (i <= t);
+            t_bloque_parcial = t(indice_mayores); % Para t
+            y_bloque_parcial = y(indice_mayores); % Para y
+            indice_menores = (t_bloque_parcial <= x_final);
+            t_bloque = t_bloque_parcial(indice_menores);  % Para t
+            y_bloque = y_bloque_parcial(indice_menores);  % Para y
+            
+            last_loop = true;
+        else
+            % Datos de BLOQUE
+            indice_mayores = (i <= t);
+            t_bloque_parcial = t(indice_mayores); % Para t
+            y_bloque_parcial = y(indice_mayores); % Para y
+            indice_menores = (t_bloque_parcial < (i + segundos_bloque));
+            t_bloque = t_bloque_parcial(indice_menores);  % Para t
+            y_bloque = y_bloque_parcial(indice_menores);  % Para y
+        end
         
+      
         % Filtro SALVITZKY para reducir Ruido (y_smooth)
-        y_smooth = sgolayfilt(y_bloque,7,21);
+        order_sgolay = 7;
+        framelen = 21;
+        % Asegurar que la cantidad de y_bloque es mayor que framelen
+        if not(length(y_bloque) > framelen)
+            order_sgolay = length(y_bloque)-2;
+            framelen = length(y_bloque)-1;
+            % Solo is es odd (impar) : order_sgolay < framelen
+            if rem(framelen,2) ~= 1
+                order_sgolay = order_sgolay-1;
+                framelen = framelen-1;
+            end
+            fprintf('Se cambio el orden de Savitzky Golay\n')
+        end
+        y_smooth = sgolayfilt(y_bloque,order_sgolay,framelen);
         % Plot señal Suavizada
         figure(cont_fig)
         subplot(4,1,1)
         plot(t_bloque,y_smooth)
         title('Señal Suavizada')
+        axis([t_bloque(1) t_bloque(end) min(y_smooth) max(y_smooth)])
+        grid on
+        
         
         % DETREND (Quitar tendencia de la señal) (y_detrend)
         [p,s,mu] = polyfit((1:numel(y_smooth))',y_smooth,6);
@@ -143,6 +185,7 @@ if tiempo_total > segundos_bloque % Por encima de 10 segundos (tiempo total)
         plot(t_bloque, y_var)
         title('Señal x Señal')
         axis([t_bloque(1) t_bloque(end) min(y_var) 1])
+        grid on
         y_normal = y_var;
         
         % NORMALIZAR             (y_normal)
@@ -165,9 +208,7 @@ if tiempo_total > segundos_bloque % Por encima de 10 segundos (tiempo total)
         % umbral minimo de pico (TEORICO)
         min_peak_value_theory = 0.2;  
         % Los picos deben ser si o si mayores a 0.29
-        if min_peak_value >= min_peak_value_theory
-            fprintf('El pico maximo es mayor a %.3f\n', min_peak_value_theory)
-        else
+        if not(min_peak_value >= min_peak_value_theory)
             fprintf('El pico maximo es menor a %.3f\n', min_peak_value_theory)
             min_peak_value = min_peak_value_theory;
         end
@@ -183,35 +224,111 @@ if tiempo_total > segundos_bloque % Por encima de 10 segundos (tiempo total)
         axis([t_bloque(1) t_bloque(end) min(y_normal) 1])
         fprintf('Numero de Picos: %d\n', length(y_peaks));
         
-        %RR-Variability
-        RRv_parcial = 0;
-        RRv = 0;
-        rateBPN_values = [];
-        rateBPN_times  = [];
-        rateBPN_sum    = 0 ;
-        if (length(y_peaks)> 9)       %minimo 10
+        
+        % RR-VARIABILITY
+        RRv_suma = 0;
+        RRv_variamucho = false;
+        minimo_variacion = 0.6;
+        
+        % RR - INTERVALOS
+        rr_values    = [];
+        rr_promedio  = 0;
+        
+        % RR-MEAN
+        fuerade_rrmean = false;
+        
+        % MINIMO 10 picos
+        if (length(y_peaks)> 9)       
+            
+            % RR - VARIABILITY
             for i2 = 1:(length(y_peaks)-2)
-                RRv_parcial = RRv_parcial + abs((t_peaks(i2+2)-t_peaks(i2+1))-(t_peaks(i2+1)-t_peaks(i2)));
+                % No deberia haber variacion (RRv = 0)
+                RRv21 = (t_peaks(i2+1)-t_peaks(i2));
+                RRv32 = (t_peaks(i2+2)-t_peaks(i2+1));
+                RRv_suma = RRv_suma + abs(RRv32 - RRv21);
+                % Intervalos RR varian mucho?
+                if RRv_suma > minimo_variacion
+                    RRv_variamucho = true;
+                end
             end
-            RRv = RRv_parcial/length(y_peaks);
-            fprintf('RRv_parcial: %.3f\n', RRv_parcial);
-            fprintf('RRv_promedio: %.3f\n', RRv);
+            
+            % RR - INTERVALOS (segundos)
+            for i3 = 2:length(t_peaks)
+                % se guarda valor intervalo RR
+                pulso_ant = t_peaks(i3-1);
+                pulso_act = t_peaks(i3);
+                rr_values = [rr_values (pulso_act - pulso_ant)];
+            end
+            rr_suma = sum(rr_values);
+            rr_promedio = sum(rr_values)/length(t_peaks);
+            fprintf('RR valores SUMA: %.3f\n', rr_suma);
+            fprintf('RR Intervalo promedio: %.3f\n', rr_promedio);
+            % Asignamos al rr_values total
+            rr_values_all = [rr_values_all rr_values];
+            
+            % MEAN R-R Interval (se toma rr_values anterior)
+            rrmean_values = [];
+            rr_mean       = 0;
+            for i4 = 1:length(rr_values)
+                rr_mean = 0.75*rr_mean+0.25*rr_values(i4);
+                rrmean_values = [rrmean_values rr_mean];
+            end
+            % Valores R-R Limites
+            up_rr_mean   = find(rr_values>=(rr_mean*1.15));
+            down_rr_mean = find(rr_values<(rr_mean*0.85));
+            if (length(up_rr_mean) + length(down_rr_mean)) > 1
+                fuerade_rrmean = true;
+            end
+            % Asignamos al rr_mean valores totales
+            rr_mean_values_all = [rr_mean_values_all rr_mean];
+            
+            % BEATS PER MIMUNTE
+            rateBPM = floor(length(y_peaks)*60/(t_bloque(end)-t_bloque(1)));
+            fprintf('BPM: %d\n', rateBPM);
 
-            %Secs entre intervalos ?? REVISAR
-            for i4 = 2:length(t_peaks)
-                rateBPN_values = [rateBPN_values t_peaks(i4)-t_peaks(i4-1)];
-            end
-            rateBPN_sum = sum(rateBPN_values);
-            fprintf('RRv_promedio: %.3f\n', rateBPN_sum);
+        end
+        
+        
+        if (fuerade_rrmean==true) || (RRv_variamucho==true)
+            fprintf('Presenta FA\n', floor(rateBPM))
+        else
+            fprintf('No presenta FA\n')
         end
 
+        
         % Para conteo de figuras
         cont_fig = cont_fig+1;
         
+        % LOOP ULTIMO
+        if last_loop
+            break
+        end
 
     end
-  
-else
-    haha='(:'
-end
     
+    %% PLOT R-R Mean
+    rr_mean_prom = sum(rr_mean_values_all)/length(rr_mean_values_all);
+    figure(cont_fig)
+    plot(rr_values_all,'g*')
+    title('R-R Total Variability')
+    ylabel('R-R Intervalos segundos')
+    xlabel('Numero de Bloques (10 segundos)')
+    % Limite RR-Mean Promedio
+    hold on
+    rrmean_plot = plot([0 length(rr_values_all)],[rr_mean_prom rr_mean_prom]);
+    xlim([0, length(rr_values_all)])
+    if rr_mean_prom > 1
+        ylim([0,1.5])
+    else
+        ylim([0,1])
+    end
+    % Limite RR-Mean Inferior
+    hold on
+    rrmean_plot = plot([0 length(rr_values_all)],[rr_mean_prom*0.85 rr_mean_prom*0.85],'--r');
+    % Limite RR-Mean Superior
+    hold on
+    rrmean_plot = plot([0 length(rr_values_all)],[rr_mean_prom*1.15 rr_mean_prom*1.15],'--r');
+
+else
+    fprinf('No se adquirio suficiente tiempo')
+end
