387f0b47a0f3630d6977a56078131b0aba6a9157
Fiorella Quino <Fiorella@MacBook-Pro-de-Fiorella.local>
2018-02-28 21:57:49 -0500

    postgrel

--- holter_project/settings.py
+++ holter_project/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'holter_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'holterdb',
+        'USER': 'holter',
+        'PASSWORD': 'upcdb123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
